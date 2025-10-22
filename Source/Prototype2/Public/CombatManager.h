2f2006a5f48556e5319f5586f1ce1ac58ab3ae15
Beast-Bourne <137620684+Beast-Bourne@users.noreply.github.com>
2025-10-22 23:01:08 +0100

    made combat manager game mode and player pawn

---
+++ Source/Prototype2/Public/CombatManager.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CombatManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTOTYPE2_API ACombatManager : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+
+private:
+	void EnablePlayerLocationPicking();
+	
+};
