555a558f3baface1165bd8dae22a14169232a24d
Beast-Bourne <137620684+Beast-Bourne@users.noreply.github.com>
2025-10-26 10:35:00 +0000

    Enemy now determines a player target at the start of its turn based on players distance from the enemy

--- Source/Prototype2/Private/EnemyEntity.cpp
+++ Source/Prototype2/Private/EnemyEntity.cpp
@@ -2,9 +2,40 @@
 
 
 #include "EnemyEntity.h"
+#include "CombatManager.h"
+#include "Kismet/GameplayStatics.h"
 
 AEnemyEntity::AEnemyEntity()
 {
 	CharMesh = GetCharMesh();
 	CharacterMesh->SetStaticMesh(CharMesh);
 }
+
+void AEnemyEntity::DeterminePlayerTarget()
+{
+	ACombatManager* CombatManager = Cast<ACombatManager>(UGameplayStatics::GetGameMode(GetWorld()));
+	
+	int ShortestDist = 100000;
+	APlayerEntity* ClosestPlayer = nullptr;
+	
+	for (AEntityBase* Entity : CombatManager->Combatants)
+	{
+		APlayerEntity* Player = Cast<APlayerEntity>(Entity);
+		if (!Player) continue;
+
+		FIntVector PlayerPos = Player->PositionCoord;
+		int XDist = abs(PlayerPos.X - PositionCoord.X);
+		int YDist = abs(PlayerPos.Y - PositionCoord.Y);
+		int ZDist = abs(PlayerPos.Z - PositionCoord.Z);
+		int totalDist = XDist + YDist + ZDist;
+
+		if (totalDist >= ShortestDist) continue;
+
+		ShortestDist = totalDist;
+		ClosestPlayer = Player;
+	}
+
+	PlayerTarget = ClosestPlayer;
+	UE_LOG(LogTemp, Warning, TEXT("Player target is: %s"), *ClosestPlayer->GetName())
+}
+
--- Source/Prototype2/Public/EnemyEntity.h
+++ Source/Prototype2/Public/EnemyEntity.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "EntityBase.h"
+#include "PlayerEntity.h"
 #include "EnemyEntity.generated.h"
 
 /**
@@ -16,7 +17,12 @@ class PROTOTYPE2_API AEnemyEntity : public AEntityBase
 public:
 	AEnemyEntity();
 
+	UPROPERTY(BlueprintReadWrite, Category = "EnemyLogic")
+	APlayerEntity* PlayerTarget;
+
 protected:
 	virtual UStaticMesh* GetCharMesh() const { return LoadObject<UStaticMesh>(nullptr, TEXT("/Game/Art/Static_Meshes/Enemies/SM_Wolf.SM_Wolf")); }
-	
+
+	UFUNCTION(BlueprintCallable)
+	void DeterminePlayerTarget();
 };
